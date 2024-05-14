4e1edea05c474e31a7a2115d4b0863197c0b2de4
joseeliasnsf <joseeliasnsf@outlook.com>
2024-05-14 18:33:18 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio padrão do formulário
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const confirmarSenha = document.getElementById('confirmarSenha').value;
+
+    
+
+    // Processamento dos dados do formulário (ex: enviar para servidor)
+    console.log('Dados do formulário:', {
+        nome: nome,
+        email: email,
+        senha: senha
+    });
+
+    // Limpar o formulário
+    formulario.reset();
+});
