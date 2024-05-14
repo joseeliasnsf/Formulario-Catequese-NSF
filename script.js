64aed6f04fd36caad611cd8dfbbf7c78ff399090
joseeliasnsf <joseeliasnsf@outlook.com>
2024-05-14 19:30:21 -0300

    Atualização

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ formulario.addEventListener('submit', function(event) {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
-    const confirmarSenha = document.getElementById('confirmarSenha').value;
+    
 
     
 
